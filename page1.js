648a3171f50119b8f1316a6d308ac499dcb727d5
iggi-71 <jimenez72iggi@gmail.com>
2020-10-19 15:14:45 -0700

    last touches

--- page1.js
+++ page1.js
@@ -12,3 +12,18 @@ document.getElementById("cash").onclick = function(){
   var cash = prompt("Enter amount");
   document.getElementById("outputCash").innerText = cash;
 }
+
+document.getElementById("name1").onclick = function(){
+	var name1 = prompt("Enter the name of PERSON you are BORROWING from");
+  document.getElementById("outputName1").innerText = name1;
+}
+
+document.getElementById("desc1").onclick = function(){
+  var desc1 = prompt("Enter Description of Transaction");
+  document.getElementById("outputDesc1").innerText = desc1;
+}
+
+document.getElementById("cash1").onclick = function(){
+  var cash1 = prompt("Enter amount");
+  document.getElementById("outputCash1").innerText = cash1;
+}
